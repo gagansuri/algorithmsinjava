8779e434c198b6ed4a085b4eba30a1bc1f6abe71
Gagan Suri <gagan.suri@pb.com>
2020-01-09 12:40:29 -0600

    gitignore

--- src/main/java/UnionFindClient.java
+++ src/main/java/UnionFindClient.java
@@ -1,3 +1,5 @@
+import com.passion.study.coursera.algorithms.part1.unionfind.UnionFind;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -12,12 +14,13 @@ public class UnionFindClient {
         );
         BufferedReader br=new BufferedReader(new FileReader(file));
         int n = Integer.parseInt(br.readLine().trim());
+        //UnionFind uf=new UnionFind(n);
         UnionFind uf=new UnionFind(n);
         String line=null;
         while((line=br.readLine())!=null) {
             int p = Integer.parseInt(line.split(" ")[0]);
             int q = Integer.parseInt(line.split(" ")[1]);
-            if(!uf.isConnected(p,q)) {
+            if(!uf.connected(p,q)) {
                 uf.union(p, q);
                 System.out.println("p->"+p+" q->"+q);
             }
--- src/main/java/com/passion/study/coursera/algorithms/part1/unionfind/UnionFind.java
+++ src/main/java/com/passion/study/coursera/algorithms/part1/unionfind/UnionFind.java
@@ -2,4 +2,45 @@ package com.passion.study.coursera.algorithms.part1.unionfind;
 
 public class UnionFind
 {
-}
+  private int id[];
+  private int N;
+  public UnionFind(int _N)
+  {
+    this.N = _N;
+    this.id = new int[this.N];
+    for (int i = 0; i < this.N; i++)
+    {
+      id[i] = i;
+    }
+  }
+    public void union(int p,int q) {
+      //read as join p to q i.e change the value of p to value of q
+      // union (2,3) set id[2] = id[3]
+      // if a node is already joined to someother node then join that as well
+      int pid=id[p];
+      int qid=id[q];
+      /*if(id[i]==pid ) {
+        id[i]=qid;
+      }*/
+
+      for(int i=0;i<this.N;i++) {
+        if(this.id[i]==pid) {
+          this.id[i]=qid;
+        }
+      }
+
+    }
+
+    public boolean connected(int p,int q) {
+      return this.id[p]==this.id[q];
+    }
+
+  public void printNodes(){
+    for(int i=0;i<this.N;i++) {
+      System.out.print(id[i] + " ");
+    }
+  }
+
+  }
+
+
